af711542f4e13f8795a504b7bdf2fe40744d3429
RajGuptaVips2025 <rajguptavips2025@gmail.com>
2025-07-07 13:19:36 +0530

    updating with main branch

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -15,6 +15,7 @@ export async function POST(request: NextRequest) {
 
       const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GENERATIVE_AI_API_KEY!);
       const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+      console.log("model");
   
       const result = await model.generateContent(messages);
       console.log("response --->  ",result.response.text(),'\n\n')
