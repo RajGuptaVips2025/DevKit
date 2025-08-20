86721ee1fc592ce9c6353441a1b76558a47d1cb1
RajGuptaVips2025 <rajguptavips2025@gmail.com>
2025-08-20 06:20:10 +0530

    env error fix

--- src/app/builder/Builder.tsx
+++ src/app/builder/Builder.tsx
@@ -262,7 +262,7 @@ export default function Builder() {
         cloudForm.append("upload_preset", "your_unsigned_preset"); // set this in Cloudinary dashboard
 
         const cloudRes = await fetch(
-          `https://api.cloudinary.com/v1_1/${process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME}/upload`,
+          `https://api.cloudinary.com/v1_1/${process.env.CLOUDINARY_CLOUD_NAME}/upload`,
           { method: "POST", body: cloudForm }
         );
         const cloudData = await cloudRes.json();
